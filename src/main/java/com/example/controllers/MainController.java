a5b051c728ebace64da59fa36a5b4b642e2ca1bd
Salma <salma.asbayti@gmail.com>
2023-03-21 17:23:21 +0100

    Temporal

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,14 +1,17 @@
 package com.example.controllers;
 
+import java.lang.ProcessBuilder.Redirect;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties.Redis;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.entities.Estudiante;
 import com.example.services.EstudianteService;
@@ -47,7 +50,14 @@ public class MainController {
 
     @PostMapping("/altaEstudiante")
     /** Metodo que recibe los dayos procedente de los controles del formulario */
-    public void altaEstudiante(){
-
+    public RedirectView altaEstudiante(){ 
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("/listar");
+    return redirectView;
     }
+    // @PostMapping("/altaEstudiante")
+    // /** Metodo que recibe los dayos procedente de los controles del formulario
+    // public String altaEstudiante(){  /* va a devolver una vista si todo ha ido correctaemnte */
+    // return "redirect:/listar"; */
+    // }
 }
