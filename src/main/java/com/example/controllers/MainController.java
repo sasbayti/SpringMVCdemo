ad766c81d30586b490f6b10d1a63416c3d75d61a
Salma <salma.asbayti@gmail.com>
2023-03-21 14:30:29 +0100

    sincronizado con la rama formulario

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -42,5 +43,11 @@ public class MainController {
         model.addAttribute("estudiante", new Estudiante());
 
         return "views/formularioAltaEstudiante";
+    }
+
+    @PostMapping("/altaEstudiante")
+    /** Metodo que recibe los dayos procedente de los controles del formulario */
+    public void altaEstudiante(){
+
     }
 }
