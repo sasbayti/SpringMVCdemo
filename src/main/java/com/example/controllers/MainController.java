78cd30aeda3ef33d286fde2628d88e6c83a6b8b1
Salma <salma.asbayti@gmail.com>
2023-03-22 13:20:55 +0100

    Encontrado que facultad es con tradicional

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -3,33 +3,40 @@ package com.example.controllers;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.entities.Estudiante;
+import com.example.entities.Facultad;
 import com.example.entities.Telefono;
 import com.example.services.EstudianteService;
+import com.example.services.FacultadService;
 import com.example.services.TelefonoService;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
-    private static final  Logger LOG = Logger.getLogger("MainController");
+    private static final Logger LOG = Logger.getLogger("MainController");
 
     @Autowired
     private EstudianteService estudianteService;
 
     @Autowired
     private TelefonoService telefonoService;
+
+    @Autowired
+    private FacultadService facultadService;
 
     /**
      * El metodo siguiente devuelve un listado de estudiantes
@@ -51,7 +58,12 @@ public class MainController {
     @GetMapping("/frmAltaEstudiante")
     public String formularioAltaEstudiante(Model model) {
 
-        model.addAttribute("estudiante", new Estudiante());
+        Estudiante estudiante = new Estudiante();
+
+        List<Facultad> facultades = facultadService.findAll();
+
+        model.addAttribute("estudiante", estudiante);
+        model.addAttribute("facultades", facultades);
 
         return "views/formularioAltaEstudiante";
     }
@@ -63,32 +75,63 @@ public class MainController {
     // redirectView.setUrl("/listar");
     // return redirectView;
     // }
-    @PostMapping("/altaEstudiante")
+    @PostMapping("/altaModificacionEstudiante")
     /** Metodo que recibe los dayos procedente de los controles del formulario */
     public String altaEstudiante(@ModelAttribute Estudiante estudiante,
-    @RequestParam(name = "numerosTelefonos") String telefonosRecibidos) { /** va a devolver una vista si todo ha ido correctamente */
-      
-      LOG.info("Los telefonos recibidos son: " + telefonosRecibidos);
-      
-      List<String> listadoNumerosTelefonos = null; // Como ha sido declarada aqui, ya no necesito hacerlo despues
+            @RequestParam(name = "numerosTelefonos") String telefonosRecibidos) { /**
+                                                                                   * va a devolver una vista si todo ha
+                                                                                   * ido correctamente
+                                                                                   */
 
-      if(telefonosRecibidos != null) {
-        String[] arrayTelefonos = telefonosRecibidos.split(";"); /* Aqui separo por punto y coma */
-       
-        listadoNumerosTelefonos = Arrays.asList(arrayTelefonos); // Aqui lo convierto automaticamente en la lista de telefonos
-  
-      }
- 
-      estudianteService.save(estudiante);
-      
-      if(listadoNumerosTelefonos != null){
-        listadoNumerosTelefonos.stream().forEach(n -> {  // n hace referencia al numero que va pasando, uno a uno
-            Telefono telefonoObject = Telefono.builder().numero(n).estudiante(estudiante)
-            .build();
+        LOG.info("Los telefonos recibidos son: " + telefonosRecibidos);
 
-            telefonoService.save(telefonoObject);
-        });
-      }
-      return "redirect:/listar";
+        List<String> listadoNumerosTelefonos = null; // Como ha sido declarada aqui, ya no necesito hacerlo despues
+
+        if (telefonosRecibidos != null) {
+            String[] arrayTelefonos = telefonosRecibidos.split(";"); /* Aqui separo por punto y coma */
+
+            listadoNumerosTelefonos = Arrays.asList(arrayTelefonos); // Aqui lo convierto automaticamente en la lista de
+                                                                     // telefonos
+
+        }
+
+        estudianteService.save(estudiante);
+
+        if (listadoNumerosTelefonos != null) {
+            listadoNumerosTelefonos.stream().forEach(n -> { // n hace referencia al numero que va pasando, uno a uno
+                Telefono telefonoObject = Telefono.builder().numero(n).estudiante(estudiante)
+                        .build();
+
+                telefonoService.save(telefonoObject);
+            });
+        }
+        return "redirect:/listar";
+    }
+
+    /**
+     * Necesito un metodo que actualiza estudiante por lo que me a mostrar el
+     * formulario
+     */
+
+    @GetMapping("/frmActualizar/{id}")
+    public String frmActualizaEstudiante(@PathVariable(name = "id") int idEstudiante, Model model) {
+        // Es una variable en la ruta (idEstudiante) para recoger el id (temporalmente)
+        // MOdel model le tenqo que pasarle el modelo, estudiante, telefonos faucltad
+        // para yo poder modificarlo
+        // La validacion de si tengo estudiante o no seria una buena practica pero no lo
+        // vamos a hacer para ir rapido
+
+        Estudiante estudiante = estudianteService.findById(idEstudiante);
+        List<Telefono> todostelefonos = telefonoService.findAll();
+        List<Telefono> telefonosDelEstudiante = todostelefonos.stream().filter(t -> t.getEstudiante()
+                .getId() == idEstudiante).collect(Collectors.toList());
+    
+     //   List<Facultad> facultades = facultadService.findAll();
+
+        model.addAttribute("estudiante", estudiante); // para que te traiga el formulario con los datos llenos
+        model.addAttribute("telefonos", telefonosDelEstudiante);
+     //   model.addAttribute("facultades", facultades);
+
+        return "views/formularioAltaEstudiante"; // quiero que vuelva a la vista
     }
 }
